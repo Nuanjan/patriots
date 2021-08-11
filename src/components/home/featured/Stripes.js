aec5e9cf5b88952e1ae71fa2fa817c2767e338d1
Nuanjan <nuanjan.schluntz@gmail.com>
2021-08-11 18:23:45 -0400

    fix responsive for feature component

--- src/components/home/featured/Stripes.js
+++ src/components/home/featured/Stripes.js
@@ -1,84 +1,87 @@
-import React, { Component } from 'react';
-import { easePolyOut } from 'd3-ease';
-import Animate from 'react-move/Animate';
+import React, { Component } from "react";
+import { easePolyOut } from "d3-ease";
+import Animate from "react-move/Animate";
 
 class Stripes extends Component {
-    state={
-        stripes: [
-            {background: 'red',
-            left:70,
-            rotate:25,
-            top:-10,
-            delay:0
+  state = {
+    stripes: [
+      {
+        background: "red",
+        left: "20%",
+        top: "-30%",
+        rotate: "25",
+        delay: 0,
+      },
+      {
+        background: "white",
+        left: "32%",
+        top: "-30%",
+        rotate: "25",
+        delay: 200,
+      },
+      {
+        background: "red",
+        left: "44%",
+        top: "-30%",
+        rotate: "25",
+        delay: 400,
+      },
+      {
+        background: "white",
+        left: "56%",
+        top: "-30%",
+        rotate: "25",
+        delay: 600,
+      },
+    ],
+  };
+  showStripes = () =>
+    this.state.stripes.map((stripe, i) => (
+      <Animate
+        key={i}
+        show={true}
+        start={{
+          background: "white",
+          opacity: 0,
+          left: 0,
+          rotate: 0,
+          top: 0,
+        }}
+        enter={{
+          background: [stripe.background],
+          opacity: [1],
+          left: [stripe.left],
+          rotate: [stripe.rotate],
+          top: [stripe.top],
+          bottom: [stripe.bottom],
+          timing: { delay: stripe.delay, duration: 200, ease: easePolyOut },
+          events: {
+            end() {
+              console.log("animation finish");
             },
-            {background: 'white',
-            left:180,
-            rotate:25,
-            top:-30,
-            delay:200
-        },
-            {background: 'red',
-            left:290,
-            rotate:25,
-            top:-30,
-            delay:400
-        },
-        {background: 'white',
-            left:400,
-            rotate:25,
-            top:-30,
-            delay:600
-        }
-        ]
-    }
-    showStripes = () => (
-        this.state.stripes.map((stripe, i) => (
-           <Animate
-           key={i}
-           show={true}
-           start={{
-               background:'white',
-               opacity:0,
-               left:0,
-               rotate:0,
-               top:0
+          },
         }}
-           enter={{
-               background:[stripe.background],
-               opacity:[1],
-               left:[stripe.left],
-               rotate:[stripe.rotate],
-               top:[stripe.top],
-               timing: {delay:stripe.delay,duration:200,ease:easePolyOut},
-               events:{
-                   end(){
-                        console.log('animation finish')
-                   }
-               }
+      >
+        {({ opacity, left, rotate, top, background, bottom }) => {
+          return (
+            <div
+              className="stripe"
+              style={{
+                background,
+                opacity,
+                position: "absolute",
+                top,
+                left,
+                transform: `rotate(${rotate}deg)`,
+              }}
+            ></div>
+          );
         }}
-           >
-               {({opacity, left,rotate, top, background}) => {
-                   return(
-                    <div
-                    className="stripe"
-                    style={{
-                        background,
-                        opacity,
-                        transform: `rotate(${rotate}deg) translate(${left}%, ${top}em)`
-                    }}
-                    ></div>
-                   );
-               }}
-           </Animate> 
-        ))
-    )
-    render() {
-        return (
-            <div className="featured_stripes">
-                {this.showStripes()}
-            </div>
-        );
-    }
+      </Animate>
+    ));
+  render() {
+    return <div className="featured_stripes">{this.showStripes()}</div>;
+  }
 }
 
 export default Stripes;
--- src/components/home/featured/Text.js
+++ src/components/home/featured/Text.js
@@ -1,127 +1,129 @@
-import React, { Component } from 'react';
-import { easePolyOut } from 'd3-ease';
-import Animate from 'react-move/Animate';
-import FeaturedPlayer from '../../../Resources/images/featuredPlayer.png'
+import React, { Component } from "react";
+import { easePolyOut } from "d3-ease";
+import Animate from "react-move/Animate";
+import FeaturedPlayer from "../../../Resources/images/featuredPlayer.png";
 class Text extends Component {
-    animateNumber =() => (
-        <Animate
-        show={true}
-        start={{
-            opacity:0,
-            rotate:0
-        }}
-        enter={{
-            opacity:[1],
-            rotate:[360],
-            timing:{duration: 1000, ease: easePolyOut}
-        }}
-        >
-            {({opacity, rotate}) => {
-                return (
-                    <div className="featured_number"
-                    style={{
-                        opacity,
-                        transform:`translate(8vw, 40%) rotateY(${rotate}deg)`
-                    }}>
-                            6
-                    </div>
-                )
-            }}
-        </Animate>
-    );
-    animateFirst = () => (
-        <Animate
-        show={true}
-        start={{
-            opacity:0,
-            x: 503,
-            y: 450
-        }}
-        enter={{
-            opacity:[1],
-            x: [8],
-            y: [350],
-            timing:{duration: 500, ease: easePolyOut}
-        }}
-        >
-            {({opacity, x, y}) => {
-                return (
-                    <div className="featured_first"
-                    style={{
-                        opacity,
-                        transform:`translate(${x}vw, ${y}%)`
-                    }}>
-                            Super Bowl
-                    </div>
-                )
-            }}
-        </Animate>
-    );
-
-    animateSecond = () => (
-        <Animate
-        show={true}
-        start={{
-            opacity:0,
-            x: 503,
-            y: 586
-        }}
-        enter={{
-            opacity:[1],
-            x: [8],
-            y: [480],
-            timing:{delay:300,duration: 500, ease: easePolyOut}
-        }}
-        >
-            {({opacity, x, y}) => {
-                return (
-                    <div className="featured_second"
-                    style={{
-                        opacity,
-                        transform:`translate(${x}vw, ${y}%)`
-                    }}>
-                            Championships
-                    </div>
-                )
-            }}
-        </Animate>
-    );
-    animatePlayer = () => (
-        <Animate
-        show={true}
-        start={{
-            opacity:0,
-        }}
-        enter={{
-            opacity:[1],
-            timing:{delay:800,duration: 500, ease: easePolyOut}
-        }}
-        >
-            {({opacity}) => {
-                return (
-                    <div className="featured_player"
-                    style={{
-                        opacity,
-                        background:`url(${FeaturedPlayer})`,
-                        transform:`translate(20vw, 30%)`
-                    }}>
-                    </div>
-                )
-            }}
-        </Animate> 
-    )
-
-    render() {
+  animateNumber = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        rotate: 0,
+      }}
+      enter={{
+        opacity: [1],
+        rotate: [360],
+        timing: { duration: 1000, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, rotate }) => {
         return (
-            <div className="featured_text">
-                {this.animatePlayer()}
-                {this.animateNumber()}
-                {this.animateFirst()}
-                {this.animateSecond()}
-                
-            </div>
+          <div
+            className="featured_number"
+            style={{
+              opacity,
+            }}
+          >
+            6
+          </div>
         );
-    }
+      }}
+    </Animate>
+  );
+  animateFirst = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 450,
+      }}
+      enter={{
+        opacity: [1],
+        x: [8],
+        y: [350],
+        timing: { duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_first"
+            style={{
+              opacity,
+            }}
+          >
+            Super Bowl
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  animateSecond = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 586,
+      }}
+      enter={{
+        opacity: [1],
+        x: [8],
+        y: [480],
+        timing: { delay: 300, duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_second"
+            style={{
+              opacity,
+            }}
+          >
+            Championships
+          </div>
+        );
+      }}
+    </Animate>
+  );
+  animatePlayer = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+      }}
+      enter={{
+        opacity: [1],
+        timing: { delay: 800, duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity }) => {
+        return (
+          <div
+            className="featured_player"
+            style={{
+              opacity,
+              background: `url(${FeaturedPlayer})`,
+            }}
+          ></div>
+        );
+      }}
+    </Animate>
+  );
+
+  render() {
+    return (
+      <div className="featured_text">
+        {this.animatePlayer()}
+        {this.animateNumber()}
+        {this.animateFirst()}
+        {this.animateSecond()}
+      </div>
+    );
+  }
 }
 
 export default Text;
